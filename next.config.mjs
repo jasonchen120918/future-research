6c62f5f64ceb84b486f26eda10dab7fd989fafb9
Temporary User <temp@example.com>
2025-05-02 09:14:54 +0800

    Update deployment configuration for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/future-research',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
